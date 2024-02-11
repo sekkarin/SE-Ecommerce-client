bb5752b9efc56a9c0a446a71e8a94888a10dc471
sekkarin <644259021@webmail.npru.ac.th>
2024-02-11 20:22:17 +0700

    finish

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import router from './Routes/Router.jsx'
+import {RouterProvider} from "react-router-dom";
+import "./App.css"
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+)
---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import Banner from "../components/Banner";
+import Categories from "./Categories";
+import SpecialProduct from "./SpecialProduct";
+import Testimonials from "./Testimonials";
+import OurService from "./OurService";
+
+const Home = () => {
+  return (
+    <div>
+      <Banner />
+      <Categories />
+      <SpecialProduct />
+      <Testimonials />
+      <OurService />
+    </div>
+  );
+};
+
+export default Home;
